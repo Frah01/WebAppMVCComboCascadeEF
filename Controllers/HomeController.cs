dbcd2d66c7e9058bbb43579713edfb1f01a80f33
Frah01 <vinario.francesco@gmail.com>
2024-02-13 13:01:21 +0100

    Serilog inserito in appsettings.json, ancora da mettere in program.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Diagnostics;
 using WebAppMVCComboCascadeEF.Models;
 
@@ -7,16 +8,61 @@ namespace WebAppMVCComboCascadeEF.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly CorsoAcademyContext _context;
+        public HomeController(CorsoAcademyContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
-            return View();
+            CascadingModel model = new CascadingModel();
+            foreach (var country in _context.TRegiones)
+            {
+                model.Regioni.Add(new SelectListItem { Text = country.Nome, Value = country.Id.ToString() });
+            }
+            return View(model);
         }
+        [HttpPost]
+        public ActionResult Index(int? IdRegione, int? IdProvincia, int? IdComune)
+        {
+            CascadingModel model = new CascadingModel();
+
+            // Popolamento delle dropdown Regioni
+            foreach (var regione in _context.TRegiones)
+            {
+                model.Regioni.Add(new SelectListItem { Text = regione.Nome, Value = regione.Id.ToString() });
+            }
+
+            // Se è stato selezionato un paese, popola le dropdown Province
+            if (IdRegione.HasValue)
+            {
+                var province = _context.TProvincia.Where(provincia => provincia.IdRegione == IdRegione.Value).ToList();
+                foreach (var provincia in province)
+                {
+                    model.Province.Add(new SelectListItem { Text = provincia.Nome, Value = provincia.Id.ToString() });
+                }
+
+                // Imposta il valore selezionato per la dropdown Regione
+                model.IdRegione = IdRegione.Value;
+
+                // Se è stato selezionato uno stato, popola le dropdown Comuni
+                if (IdComune.HasValue)
+                {
+                    var cities = _context.TComunes.Where(city => city.IdProvincia == IdComune.Value).ToList();
+                    foreach (var city in cities)
+                    {
+                        model.Comuni.Add(new SelectListItem { Text = city.Nome, Value = city.Id.ToString() });
+                    }
+
+                    // Imposta il valore selezionato per la dropdown Provincia
+                    model.IdComune = IdComune.Value;
+                }
+            }
+
+            return View(model);
+        }
+
 
         public IActionResult Privacy()
         {
---
+++ Models/CascadingModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebAppMVCComboCascadeEF.Models
+{
+    public class CascadingModel
+    {
+        public CascadingModel()
+        {
+            this.Regioni = new List<SelectListItem>();
+            this.Province = new List<SelectListItem>();
+            this.Comuni = new List<SelectListItem>();
+        }
+
+        public List<SelectListItem> Regioni { get; set; }
+        public List<SelectListItem> Province { get; set; }
+        public List<SelectListItem> Comuni { get; set; }
+
+        public int IdRegione { get; set; }
+        public int IdProvincia { get; set; }
+        public int IdComune { get; set; }
+    }
+}
+
