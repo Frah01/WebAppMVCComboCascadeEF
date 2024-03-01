47b77885c92aafec592b0b9479a095be07aec67e
Frah01 <vinario.francesco@gmail.com>
2024-03-01 11:34:21 +0100

    Implementato Progetto Database nella solution

---
+++ DatabaseCorsoAcademy/Security/CorsoAcademy_1.sql
@@ -0,0 +1,3 @@
+﻿CREATE LOGIN [CorsoAcademy]
+    WITH PASSWORD = N'yeNgh?|uTkBOfhLJxksmr~0hmsFT7_&#$!~<dkcm9b|Eaoaj', SID = 0x4AC4E9C86200AE4FA872000CE15FB678, DEFAULT_LANGUAGE = [Italiano], CHECK_POLICY = OFF;
+
